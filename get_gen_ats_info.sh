002b1a416cd575f44a652cdb5171be7f5465113a
Robert Rice <rice.robert.t@gmail.com>
2022-07-10 08:57:07 -0700

    Added README and updated script to parameterize wifi interface

--- get_gen_ats_info.sh
+++ get_gen_ats_info.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-curl --interface wlx90916930190e -s 192.168.0.90/get_info
+## The name of your wifi interface connected to the 'Champion_XXXX' SSID
+## as defined by `sudo iw dev`
+INTERFACE="wlx90916930190e"
+
+## The info URL is statically defined as 192.168.0.90/get_info
+## This IPv4 address cannot be changed
+curl --interface ${INTERFACE} -s 192.168.0.90/get_info
